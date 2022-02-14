9d5e9dbe37f99192c4287ece154a0c92032feb33
khanhxuanho <59435285+khanhxuanho@users.noreply.github.com>
2022-02-14 08:20:31 +0700

    khanh
    a

---
+++ Luyen_html_css_js/Luyen_html_css_js/Luyen_fontend/Bai2_Tag_danh_sach_sp/baith2.js
@@ -0,0 +1,31 @@
+﻿
+function changeProductList(type, element)/*ben the co this lay chinh no*/
+{
+    /*dua tham so de biet chon cai nao biet click ao cai nao*/
+    let tabs = document.getElementsByClassName('tab-item');
+
+    for (i = 0; i < tabs.length; i++) {
+        tabs[i].style.background = '#b14c38';//ve mau ban dau
+    }
+    element.style.background = '#ee4d2d';
+
+    document.getElementById(type).style.display = 'block';//an di
+    //viet de an vo tab hien sp moi o duoi
+    switch (type) {
+        case 'trend':
+            document.getElementById('new').style.display = 'none';//an di
+            document.getElementById('best-sell').style.display = 'none';//an di
+            break;
+
+        case 'new':
+            document.getElementById('trend').style.display = 'none';//an di
+            document.getElementById('best-sell').style.display = 'none';//an di
+            break;
+
+        case 'best-sell':
+            document.getElementById('trend').style.display = 'none';//an di
+            document.getElementById('new').style.display = 'none';//an di
+            
+            break;
+    }
+}
---
+++ Luyen_html_css_js/Luyen_html_css_js/Luyen_fontend/Bai8_luyen_js_DOM_co_ban/Thaydoianh_sp.js
@@ -0,0 +1,8 @@
+﻿
+function changeImage(id) {/* lay id*/
+    let image = document.getElementById(id).getAttribute('src');
+    /* lay dc link anh console.log(image);*/
+
+    document.getElementById('img-main').setAttribute('src', image);
+
+}
